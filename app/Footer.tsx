0c5d6e9aba715fd4eed49ec322bc13a897c2c24e
gondola <dr.wheats@gmail.com>
2024-06-11 18:38:40 -0400

    almost

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -4,7 +4,7 @@ function Footer(props) {
     return (
         <footer className="mb-10 px-4 text-center text-gray-500">
             <small className="mb-2 block text-xs">
-                &copy; {new Date().getFullYear()} {new Date().getFullYear()}
+                &copy; Ben dot Place {new Date().getFullYear()}
             </small>
             <p className="text-xs">
                 <span className="font-semibold">About this website:</span> built with React and Next.js, Typescript, Tailwind CSS, Framer Motion, React Email and Resend.
