37391b7206e390b7313238283e236f8bfdc69461
gondola <dr.wheats@gmail.com>
2024-06-11 18:42:16 -0400

    ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Intro from "@/components/Intro";
 import SectionDivider from "@/components/sectionDivider";
-import AboutMe from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import ExperienceSection from "@/components/experienceSection";
@@ -11,7 +10,6 @@ export default function Home() {
     <main className="flex flex-col items-center px-4">
       <Intro />
         <SectionDivider />
-        {/*<AboutMe />*/}
         <Projects />
         <Skills />
         <ExperienceSection />
