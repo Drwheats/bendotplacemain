e79c4d0e840aff90565c920291371bd5f83aa5c7
Drwheats <63183755+Drwheats@users.noreply.github.com>
2026-02-10 20:39:29 -0500

    Update matrix-client.js

--- pages/api/matrix-client.js
+++ pages/api/matrix-client.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
-  // Matrix spec expects JSON
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Disposition", "inline"); // Force browser to render JSON
   res.status(200).json({
     "m.homeserver": {
       "base_url": "https://matrix.ben.place"
